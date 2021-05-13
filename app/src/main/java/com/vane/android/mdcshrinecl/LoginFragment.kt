7bfd9c225ce9af544aff9e67042683921f5da5aa
ivVane <iv.vane@gmail.com>
2021-05-13 22:14:44 +0200

    Setting up navigation in MainActivity.kt and creating NavigationHost.kt interface

---
+++ app/src/main/java/com/vane/android/mdcshrinecl/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.vane.android.mdcshrinecl
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+}
--- app/src/main/java/com/vane/android/mdcshrinecl/MainActivity.kt
+++ app/src/main/java/com/vane/android/mdcshrinecl/MainActivity.kt
@@ -2,10 +2,32 @@ package com.vane.android.mdcshrinecl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationHost {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, LoginFragment())
+                .commit()
+        }
     }
+
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
+    }
+
 }
