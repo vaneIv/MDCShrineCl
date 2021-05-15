958015170da35ee1868256ab5a06cfe5704fcd09
ivVane <iv.vane@gmail.com>
2021-05-15 22:27:42 +0200

    Creating the layout for the RecyclerView(product_card.xml), ProductCardViewHolder.kt and ProductCardRecyclerViewAdapter.kt

---
+++ app/src/main/java/com/vane/android/mdcshrinecl/ProductCardRecyclerViewAdapter.kt
@@ -0,0 +1,30 @@
+package com.vane.android.mdcshrinecl
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vane.android.mdcshrinecl.network.ProductEntry
+
+/**
+ * Adapter used to show a simple grid of products.
+ */
+class ProductCardRecyclerViewAdapter(private val productList: List<ProductEntry>) :
+    RecyclerView.Adapter<ProductCardViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductCardViewHolder {
+        val layoutView =
+            LayoutInflater.from(parent.context).inflate(R.layout.product_card, parent, false)
+
+        return ProductCardViewHolder(layoutView)
+    }
+
+    override fun onBindViewHolder(holder: ProductCardViewHolder, position: Int) {
+        // TODO: Put ViewHolder binding code here in MDC-102
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+
+}
