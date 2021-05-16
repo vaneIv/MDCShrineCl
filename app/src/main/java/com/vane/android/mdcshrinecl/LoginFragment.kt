65d9a7ac02827de1e74a80542d8071b1c3cbc9a6
ivVane <iv.vane@gmail.com>
2021-05-16 04:16:10 +0200

    Configuring the navigation from fragment_login to product_grid_fragment

--- app/src/main/java/com/vane/android/mdcshrinecl/LoginFragment.kt
+++ app/src/main/java/com/vane/android/mdcshrinecl/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.vane.android.mdcshrinecl
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -16,14 +19,35 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
-        // Snippet from "Navigate to the next Fragment" section goes here.
-
+        // Inflate the layout for this fragment.
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        // Set an error if the password is less then 8 characters.
+        view.next_button.setOnClickListener({
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                password_text_input.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        })
+
+        // Clear the error once more then 8 characters are typed.
+        view.password_edit_text.setOnKeyListener({ _, _, _ ->
+            if (isPasswordValid(password_edit_text.text!!)) {
+                // Clear the error.
+                password_text_input.error = null
+            }
+            false
+        })
 
         return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 }
