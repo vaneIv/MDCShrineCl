dcfda22547dab160674fa7d78536825428c01e5e
ivVane <iv.vane@gmail.com>
2021-05-16 21:23:59 +0200

    Adding appbar to product_grid_fragment.xml

--- app/src/main/java/com/vane/android/mdcshrinecl/ProductGridFragment.kt
+++ app/src/main/java/com/vane/android/mdcshrinecl/ProductGridFragment.kt
@@ -1,18 +1,34 @@
 package com.vane.android.mdcshrinecl
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.product_grid_fragment, container, false)
+        // Inflate the layout for this fragment with the ProductGrid theme
+        val view = inflater.inflate(R.layout.product_grid_fragment, container, false)
+
+        // Set up the toolbar.
+        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, menuInflater)
     }
 }
